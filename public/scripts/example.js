9a7b8f6b46ed06c6cccd81398de87ce4a9a49503
zhongzf <zhongzf@126.com>
2016-08-27 22:05:36 +0800

    UISideBar

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -232,28 +232,75 @@ var UIAccordion = React.createClass({
     );
   }
 });
+
+var UISidebarItem = React.createClass({
+  render:function() {
+    return (
+      <a className="item">
+        {this.props.children}
+      </a>
+    );
+  }
+});
+
+var UISidebarPusher = React.createClass({
+  render:function() {
+    return (
+      <div className="pusher">
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var UISidebar  = React.createClass({
+  componentDidMount: function () {
+    $(this.refs.sidebar).sidebar('toggle');
+  },
+  componentWillUnmount: function () {
+    // Clean up work here.
+  },
+  shouldComponentUpdate: function () {
+    // Let's just never update this component again.
+    return false;
+  },
+  render: function () {
+    return (
+      <div ref="sidebar" className="ui sidebar menu inverted vertical">
+        {this.props.children}
+      </div>
+    );
+  }
+});
 /*********************************************************************************************/
 
 var breadcrumbData = ["Home", "Store", "T-Shirt"];
 
 ReactDOM.render(
   <div>
-    <UIButton>Follow</UIButton>
-    <UIInput type="text" placeholder="Search..." />
-    <UILabel icon="mail"> 23</UILabel>
-    <UIBreadcrumb data={breadcrumbData} />
-    <UIAccordion>
-      <UIAccordionItem title="What is a dog?">
-        <p>A dog is a type of domesticated animal.Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.</p>
-      </UIAccordionItem>
-      <UIAccordionItem title="What kinds of dogs are there?">
-        <p>There are many breeds of dogs.Each breed varies in size and temperament.Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.</p>
-      </UIAccordionItem>
-      <UIAccordionItem title="How do you acquire a dog?">
-        <p>Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.</p>
-        <p>A pet shop may be the most convenient way to buy a dog. Buying a dog from a private owner allows you to assess the pedigree and upbringing of your dog before choosing to take it home. Lastly, finding your dog from a shelter, helps give a good home to a dog who may not find one so readily.</p>
-      </UIAccordionItem>
-    </UIAccordion>
+    <UISidebar>
+      <UISidebarItem>1</UISidebarItem>
+      <UISidebarItem>2</UISidebarItem>
+      <UISidebarItem>3</UISidebarItem>
+    </UISidebar>
+    <UISidebarPusher>
+      <UIButton>Follow</UIButton>
+      <UIInput type="text" placeholder="Search..." />
+      <UILabel icon="mail"> 23</UILabel>
+      <UIBreadcrumb data={breadcrumbData} />
+      <UIAccordion>
+        <UIAccordionItem title="What is a dog?">
+          <p>A dog is a type of domesticated animal.Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.</p>
+        </UIAccordionItem>
+        <UIAccordionItem title="What kinds of dogs are there?">
+          <p>There are many breeds of dogs.Each breed varies in size and temperament.Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.</p>
+        </UIAccordionItem>
+        <UIAccordionItem title="How do you acquire a dog?">
+          <p>Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.</p>
+          <p>A pet shop may be the most convenient way to buy a dog. Buying a dog from a private owner allows you to assess the pedigree and upbringing of your dog before choosing to take it home. Lastly, finding your dog from a shelter, helps give a good home to a dog who may not find one so readily.</p>
+        </UIAccordionItem>
+      </UIAccordion>
+    </UISidebarPusher>
   </div>,
   document.getElementById('content')
 );
